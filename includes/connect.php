96faef4a13051d4d9efa47c9af97da9c0cf23c68
jankennet <jkbelano69@gmail.com>
2025-12-07 20:00:10 +0800

    "Double declare fix"

--- includes/connect.php
+++ includes/connect.php
@@ -5,14 +5,17 @@ const USER = 'root'; //Identity for authorization to access database
 const PWD = '';
 const DBNAME = 'belano_visitor_system';
 
-function ConnectDB() {
-    
-    $conn = new mysqli(HOST, USER, PWD, DBNAME);
-    if($conn -> connect_error) {
-        die('Error Connection');
-        exit;
-    }
+if (!function_exists('ConnectDB')) {
+    function ConnectDB() {
+        
+        $conn = new mysqli(HOST, USER, PWD, DBNAME);
+        if($conn -> connect_error) {
+            die('Error Connection');
+            exit;
+        }
 
-    return $conn;
+        return $conn;
+    }
 }
 
+?>
